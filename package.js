bf35281f7eee6f385ba4c634b42fb5721c6d8b1b
Marco de Jongh <marco.dejongh@gfk.com>
2014-06-16 14:52:04 +0200

    Put package.js in a closure added use strict

--- package.js
+++ package.js
@@ -1,21 +1,24 @@
+(function () {
+    'use strict';
     Npm.depends({
         'mailgun-js': '0.4.13'
     });
 
-Package.describe({
-    summary: "Mailgun API - Mailgun api implementation for Meteor."
-});
+    Package.describe({
+        summary: "Mailgun API - Mailgun api implementation for Meteor."
+    });
 
-Package.on_use(function(api) {
-    // Allow us to detect 'insecure'.
-    api.use('insecure', {weak: true});
+    Package.on_use(function(api) {
+        // Allow us to detect 'insecure'.
+        api.use('insecure', {weak: true});
 
-    api.add_files('mailgun-api.js', ['server']);
-    api.export && api.export('Mailgun', ['server']);
-});
+        api.add_files('mailgun-api.js', ['server']);
+        api.export('Mailgun', ['server']);
+    });
 
-Package.on_test(function (api) {
-    api.use(['mailgun-api', 'tinytest']);
+    Package.on_test(function (api) {
+        api.use(['mailgun-api', 'tinytest']);
 
-    api.add_files('mailgun-api_tests.js', ['server']);
-});
+        api.add_files('mailgun-api_tests.js', ['server']);
+    });
+} ());
